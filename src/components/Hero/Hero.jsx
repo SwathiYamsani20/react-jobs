52b6556e947440ae3c4e257f8558d00994b09760
swathiYamsani20 <yamswathi.ys@gmail.com>
2024-10-05 19:14:56 +0530

    trigger

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
     <div className='hero-section'>
       <h1>Become a React Dev</h1>
       <p>
-        Find the React Job that fits your skill set
+        Find the React Job that fits your skill sets
       </p>
     </div>
   )
